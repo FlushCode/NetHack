a376eb257d661c5e693e37e7150dd93f9e4e1f3a
FlushCode <pinto824@hotmail.com>
2016-08-15 18:26:39 -0300

    Add files via upload

---
+++ actores.py
@@ -0,0 +1,161 @@
+#coding=utf-8
+import curses
+
+import random
+class Actor(object):
+    """Clase base para todas las entidades capaces de ocupar una posicion en el mapa."""
+
+    def __init__(self):
+        """Inicializa el Actor."""
+        
+        self.x = 0
+        self.y = 0
+        self.vivo = True
+        
+    def dibujar(self):
+        """Devuelve el caracter que representa al Actor."""
+        
+        return '?'
+       
+
+    def jugar_turno(self, juego):
+        """Jugar un turno. `juego` es la instancia del `Juego`."""
+        
+        pass
+        
+    def interactuar_con_heroe(self, juego):
+        """Realiza la accion correspondiente a la interaccion con el heroe (es
+        decir, cuando el heroe intenta moverse a la posicion ocupada por este actor).
+        Devuelve True si el heroe realizo alguna accion, False en caso contrario."""
+        
+        return False
+             
+    def esta_vivo(self):
+        """Devuelve True en caso de estar vivo, False en caso contrario.
+        Cuando un actor informa que no esta vivo, es eliminado del mapa
+        automaticamente."""
+        
+        return self.vivo
+        
+    def es_heroe(self):
+        """Devuelve True si es el heroe del juego (es decir, si es controlado
+        por la entrada del usuario)."""
+        
+        return False
+        
+class Heroe(Actor):
+    """Representa al Heroe del juego."""
+
+    direcciones = {
+            curses.KEY_UP: (0, -1),
+            curses.KEY_LEFT: (-1, 0),
+            curses.KEY_DOWN: (0, 1),
+            curses.KEY_RIGHT: (1, 0),
+    }
+
+    def __init__(self):
+        """Crea al Heroe."""
+        self.monedas = 0
+        Actor.__init__(self)
+        
+    def es_heroe(self):
+        """Devuelve True si es el heroe del juego (es decir, si es controlado
+        por la entrada del usuario)."""
+        
+        return True
+       
+    def dibujar(self):
+        """Devuelve el caracter que representa al Heroe."""
+        
+        return '@'
+        
+    def accion(self, dx, dy, juego):
+        """Intenta realizar una accion con la celda que esta en la direccion (dx, dy).
+        Devuelve True si se realizo alguna accion, False en caso contrario."""
+        
+        mapa = juego.mapa
+        x = max(0, min(mapa.ancho() - 1, self.x + dx))
+        y = max(0, min(mapa.alto() - 1, self.y + dy))
+        if not mapa.posicion_valida(x, y):
+            return False
+
+        actor = mapa.get_celda(x, y)
+        if actor:
+            return actor.interactuar_con_heroe(juego)
+        mapa.mover_actor(self, x, y)
+        return True
+
+    def jugar_turno_heroe(self, evento, juego):
+        """Jugar un turno. `evento` es un evento de la libreria `curses` que permite
+        reaccionar ante la entrada del usuario. `juego` es una instancia `Juego`.
+        Devuelve True si se hizo alguna accion, False en caso contrario."""
+        
+        if evento in Heroe.direcciones:
+            dx, dy = Heroe.direcciones[evento]
+            return self.accion(dx, dy, juego)
+        return False
+
+class Enemigo(Actor):
+    """Representa a los enemigos del juego"""
+    def __init__(self,dibujo,nombre):
+	"""Inicializa a un enemigo"""
+	Actor.__init__(self)
+	self.nombre = nombre
+	self.dibujo = dibujo
+	
+    def interactuar_con_heroe(self,juego):
+        """Realiza la accion correspondiente a la interaccion con el heroe (es
+        decir, cuando el heroe intenta moverse a la posicion ocupada por este actor).
+        elimina al enemigo del mapa, muestra un mensaje informativo de esto y si
+        el enemigo tiene en su atributo 'tira_monedas' una referencia al numero
+        '1', se crea una nueva instancia de la moneda y se agrega donde el enemigo
+        fue previamente eliminado
+        """
+	mapa = juego.mapa
+	self.vivo = False
+	juego.msg("Has matado a un {}".format(self.nombre))
+        if random.randint(0,1) == 1:
+            moneda = Moneda()
+            mapa.agregar_actor(moneda,self.x,self.y)
+        return True
+
+    def dibujar(self):
+        return self.dibujo
+    
+class Moneda(Actor):
+    def dibujar(self):
+        """Devuelve el caracter que representa una moneda"""
+        return '$'
+    def interactuar_con_heroe(self,juego):
+        """Realiza la accion correspondiente a la interaccion con el heroe (es
+        decir, cuando el heroe intenta moverse a la posicion ocupada por este actor).
+        se elimina la moneda del mapa, se modifica el atributo monedas del heroe y
+        se muestra un mensaje por pantalla mostrando la cantidad de monedas que
+        tiene el heroe
+        """
+        mapa = juego.mapa
+        self.vivo = False
+        juego.heroe.monedas+=1
+        juego.msg("Has obtenido una moneda, total de monedas obtenidas:{}".format(juego.heroe.monedas))
+        return True
+class Pared(Actor):
+    """Representa a las paredes del juego"""
+    def dibujar(self):
+        """Devuelve el caracter que representa una pared"""
+        return "#"
+class Salida(Actor):
+    """Representa a la salida del calabozo"""
+    def dibujar(self):
+        """Devuelve el caracter que representa la salida"""
+        return "<"
+    def interactuar_con_heroe(self,juego):
+        """Realiza la accion correspondiente a la interaccion con el heroe (es
+        decir, cuando el heroe intenta moverse a la posicion ocupada por este actor).
+        se elimina al heroe del mapa, se muestran mensajes por pantalla y se finaliza
+        el juego
+        """
+        mapa = juego.mapa
+        juego.msg("Felicidades!, has salido del calabozo!")
+        juego.msg("El juego ha terminado, presiona Q para volver a la consola")
+        juego.terminar()
+        return True
---
+++ juego.py
@@ -0,0 +1,170 @@
+# -*- coding: cp1252 -*-
+import curses
+import actores
+import errores
+from mapa import Mapa
+import sys
+
+class Juego(object):
+    def __init__(self, nombre_mapa):
+        """Carga el mapa del juego a partir del archivo mapas/<nombre_mapa>.map."""
+        
+        with open("mapas/{}.map".format(nombre_mapa)) as f:
+            filas = [ linea.strip() for linea in f ]
+        mapa, heroe = self.crear_mapa(filas)
+        self.mapa = mapa
+        self.heroe = heroe
+        self.juego_terminado = False
+        # El historial de mensajes es simplemente una lista de strings.
+        self.mensajes = []
+
+    def agregar_heroe(self,mapa,x,y):
+        heroe = actores.Heroe()
+        mapa.agregar_actor(heroe,x,y)
+        return heroe
+    def agregar_goblin(self,mapa,x,y):
+        """Agrega un goblin al mapa"""
+        goblin = actores.Enemigo("g","Goblin")
+        mapa.agregar_actor(goblin,x,y)
+        
+    def agregar_orco(self,mapa,x,y):
+        """Agrega un orco al mapa"""
+        orco = actores.Enemigo("o","Orco")
+        mapa.agregar_actor(orco,x,y)
+        
+    def agregar_pared(self,mapa,x,y):
+        """Agrega una pared al mapa"""
+        pared = actores.Pared()
+        mapa.agregar_actor(pared,x,y)
+        
+    def agregar_salida(self,mapa,x,y):
+        """Agrega una salida al mapa"""
+        salida = actores.Salida()
+        mapa.agregar_actor(salida,x,y)
+      
+    def crear_mapa(self, filas):
+        """Crea el Mapa a partir de la definicion provista por parametro (`filas`),
+        y devuelve una tupla con el mapa y el actor que representa al heroe del juego.
+        `filas` es una lista de strings, que corresponde con el contenido completo
+        del archivo .map.Se presupone que en el mapa se proporciona una salida para
+        poder finalizar el juego, y si se proporciona algun caracter que no corresponde
+        a ningun personaje, se lo reemplaza por una celda vacía automáticamente.
+        Si hay mas de un heroe en la definicion del mapa, o bien si
+        no hay ningun heroe, la funcion lanza una excepcion, si el mapa no tiene
+        el formato correcto(debe ser rectangular), también
+        """
+        
+        dic_funciones = {"#":Juego.agregar_pared,
+                         "o":Juego.agregar_orco,
+                         "g":Juego.agregar_goblin,
+                         "<":Juego.agregar_salida,
+                         "@":Juego.agregar_heroe
+                         }
+        alto = len(filas)
+        ancho = len(filas[0])
+        if ancho == 0: raise IndexError("Linea vacia")
+        mapa = Mapa(ancho,alto)
+        columna_fila = 0
+        #'columna_fila' es un contador que hace referencia a el caracter actual de elemento_filas
+        elemento_filas = -1
+        #'elemento_filas' es un contador que hace referencia la cadena actual de la lista 'filas'
+        heroe = None
+        cantidad_heroes_max = 1
+        cantidad_heroes = 0
+        for fila in filas:
+            if len(fila) != ancho:
+                raise errores.MapaIncorrectoError("la linea",fila,"no contiene la misma cantidad de caracteres que la primer linea del archivo")
+            columna_fila = 0
+            elemento_filas+= 1
+            for caracter in fila:
+                if caracter in dic_funciones:
+                    nuevo_actor = dic_funciones[caracter](self,mapa,columna_fila,elemento_filas)
+                    if nuevo_actor != None:
+                        heroe = nuevo_actor
+                        cantidad_heroes+=1
+                        if cantidad_heroes > cantidad_heroes_max:
+                            raise errores.DemasiadosHeroesError("La cantidad de heroes debe ser",cantidad_heroes_max)
+            	columna_fila+=1
+            
+        if heroe == None:
+            raise errores.NoHayHeroeError("No hay ningun heroe (@) en el mapa")
+        
+        return mapa, heroe
+
+    def turno(self, evento):
+        """Llama al metodo jugar_turno() del heroe. Si el heroe hizo algo,
+        llama al metodo jugar_turno() para el resto de los actores"""
+        if self.juego_terminado:
+            return
+
+        if not self.heroe.jugar_turno_heroe(evento, self):
+            # El heroe no hizo nada
+            return
+
+        # El resto de los actores juegan su turno
+        for actor in self.mapa.actores:
+            if not actor.es_heroe():
+                actor.jugar_turno(self)
+
+        self.mapa.eliminar_actores_muertos()
+
+    def terminar(self):
+        """Marcar que el juego ha terminado."""
+        self.juego_terminado = True
+
+    def msg(self, *args):
+        """Agregar un mensaje al historial."""
+        self.mensajes.append(" ".join(args))
+
+    def dibujar_mensajes(self, ventana, cantidad):
+        """Dibujar los ultimos `cantidad` mensajes del historial en la ventana provista."""
+        for y, mensaje in enumerate(self.mensajes[-cantidad:]):
+            ventana.addstr(y, 0, mensaje)
+
+    def main(self):
+        """Bucle principal del juego."""
+        try:
+            pantalla = curses.initscr()
+
+            # No imprimir la entrada del teclado:
+            curses.noecho()
+
+            # Cursor invisible:
+            curses.curs_set(0)
+
+            # Habilitar las flechas para moverse
+            pantalla.keypad(1)
+
+            self.msg("Has ingresado en el calabozo! Podras escapar?")
+            self.msg("Flechas para moverse, Q para salir")
+            while True:
+                pantalla.clear()
+                self.mapa.dibujar(pantalla.derwin(0, 0))
+                self.dibujar_mensajes(pantalla.derwin(0, self.mapa.ancho() + 2), self.mapa.alto())
+
+                evento = pantalla.getch()
+                if evento == ord("q"):
+                    break
+                self.turno(evento)
+       
+        finally:
+            # devolver el estado de la consola (por ejemplo la visibilidad del cursor)
+            curses.endwin()
+
+
+# Mapa por defecto:
+nombre_mapa = 'nivel1'
+if len(sys.argv) > 1:
+    nombre_mapa = sys.argv[1]
+try:
+    Juego(nombre_mapa).main()
+except errores.NoHayHeroeError:
+    print "Debe haber un heroe representado por '@' para que juege el usuario."
+except errores.MapaIncorrectoError:
+    print "Formato del mapa incorrecto, debe ser rectangular"
+except errores.DemasiadosHeroesError:
+    print "Se estan añadiendo heroes de mas, solo puede haber uno"
+except IOError:
+    print "Archivo del mapa o directorio inexistentes, o no se tienen los permisos necesarios."
+except IndexError:
+    print "Mapa vacio, o no esta alineado en la izquierda superior del archivo"
